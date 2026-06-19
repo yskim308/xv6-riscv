28fd30d34c3ec45271a976aa797af2d7e07e84de
Young Seo Kim <youngseokim7@gmail.com>
2026-06-19 16:16:49 +0900

    implemente lottery scheduling

--- kernel/proc.c
+++ kernel/proc.c
@@ -451,6 +451,40 @@ kwait(uint64 addr)
   }
 }
 
+int
+do_rand(unsigned long *ctx)
+{
+  /*
+   * Compute x = (7^5 * x) mod (2^31 - 1)
+   * without overflowing 31 bits:
+   *      (2^31 - 1) = 127773 * (7^5) + 2836
+   * From "Random number generators: good ones are hard to find",
+   * Park and Miller, Communications of the ACM, vol. 31, no. 10,
+   * October 1988, p. 1195.
+   */
+  long hi, lo, x;
+
+  /* Transform to [1, 0x7ffffffe] range. */
+  x = (*ctx % 0x7ffffffe) + 1;
+  hi = x / 127773;
+  lo = x % 127773;
+  x = 16807 * lo - 2836 * hi;
+  if (x < 0)
+    x += 0x7fffffff;
+  /* Transform to [0, 0x7ffffffd] range. */
+  x--;
+  *ctx = x;
+  return (x);
+}
+
+unsigned long rand_next = 1;
+
+int
+rand(void)
+{
+  return (do_rand(&rand_next));
+}
+
 // Per-CPU process scheduler.
 // Each CPU calls scheduler() after setting itself up.
 // Scheduler never returns.  It loops, doing:
@@ -475,23 +509,57 @@ scheduler(void)
     intr_off();
 
     int found = 0;
+
+    int total_tickets = 0;
     for (p = proc; p < &proc[NPROC]; p++) {
       acquire(&p->lock);
       if (p->state == RUNNABLE) {
-        // Switch to chosen process.  It is the process's job
-        // to release its lock and then reacquire it
-        // before jumping back to us.
-        p->state = RUNNING;
-        c->proc = p;
-        swtch(&c->context, &p->context);
-
-        // Process is done running for now.
-        // It should have changed its p->state before coming back.
-        c->proc = 0;
-        found = 1;
+        total_tickets += p->tickets;
       }
       release(&p->lock);
     }
+
+    int counter = 0;
+    int winner = rand() % total_tickets;
+    for (p = proc; p < &proc[NPROC]; p++) {
+      acquire(&p->lock);
+      if (p->state == RUNNABLE) {
+        counter += p->tickets;
+        if (counter > winner) {
+          break;
+        }
+      }
+      release(&p->lock);
+    }
+
+    // at least one proccess found
+    if (counter != 0) {
+      p->state = RUNNING;
+      c->proc = p;
+      swtch(&c->context, &p->context);
+      p->ticks += 1;
+
+      c->proc = 0;
+      found = 1;
+      release(&p->lock);
+    }
+    // for (p = proc; p < &proc[NPROC]; p++) {
+    //   acquire(&p->lock);
+    //   if (p->state == RUNNABLE) {
+    //     // Switch to chosen process.  It is the process's job
+    //     // to release its lock and then reacquire it
+    //     // before jumping back to us.
+    //     p->state = RUNNING;
+    //     c->proc = p;
+    //     swtch(&c->context, &p->context);
+    //
+    //     // Process is done running for now.
+    //     // It should have changed its p->state before coming back.
+    //     c->proc = 0;
+    //     found = 1;
+    //   }
+    //   release(&p->lock);
+    // }
     if (found == 0) {
       // nothing to run; stop running on this core until an interrupt.
       asm volatile("wfi");
--- kernel/trap.c
+++ kernel/trap.c
@@ -82,8 +82,13 @@ usertrap(void)
     kexit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if (which_dev == 2)
+  if (which_dev == 2) {
+    struct proc *p = myproc();
+    acquire(&p->lock);
+    p->ticks += 1;
+    release(&p->lock);
     yield();
+  }
 
   prepare_return();
 
@@ -154,8 +159,15 @@ kerneltrap()
   }
 
   // give up the CPU if this is a timer interrupt.
-  if (which_dev == 2 && myproc() != 0)
+  if (which_dev == 2 && myproc() != 0) {
+    struct proc *p = myproc();
+    if (p) {
+      acquire(&p->lock);
+      p->ticks += 1;
+      release(&p->lock);
+    }
     yield();
+  }
 
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
