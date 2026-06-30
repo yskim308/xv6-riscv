b06ebdb3336ec296ca085faf505000eb6d12b6f1
Young Seo Kim <youngseokim7@gmail.com>
2026-06-30 18:32:36 +0900

    make first page of virtual memory space invalid

--- kernel/exec.c
+++ kernel/exec.c
@@ -29,7 +29,7 @@ kexec(char *path, char **argv)
 {
   char *s, *last;
   int i, off;
-  uint64 argc, sz = 0, sp, ustack[MAXARG], stackbase;
+  uint64 argc, sz = PGSIZE, sp, ustack[MAXARG], stackbase;
   struct elfhdr elf;
   struct inode *ip;
   struct proghdr ph;
